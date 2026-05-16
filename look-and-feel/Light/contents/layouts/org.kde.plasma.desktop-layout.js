1b163de1ba4ae6870e59ecd7238beacd647fdc68
SkyShadowHero <a1233456333@163.com>
2026-05-17 00:05:45 +0800

    feat: 添加look-and-feel

---
+++ look-and-feel/Light/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,166 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1707x1067": "",
+                    "ItemGeometries-2048x1280": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.slideshow"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "630",
+                    "DialogWidth": "810"
+                },
+                "/General": {
+                    "changedPositions": "{\"desktop:/HMCL.desktop\":[\"1707x1067\",\"0\",\"16\"],\"desktop:/Marvel Spider-Man\":[\"1707x1067\",\"4\",\"16\"],\"desktop:/android-studio-canary.desktop\":[\"1707x1067\",\"0\",\"9\"],\"desktop:/code.desktop\":[\"1707x1067\",\"0\",\"8\"],\"desktop:/computer.desktop\":[\"1707x1067\",\"0\",\"0\"],\"desktop:/feishu.desktop\":[\"1707x1067\",\"8\",\"0\"],\"desktop:/firefox.desktop\":[\"1707x1067\",\"2\",\"0\"],\"desktop:/open-orpheus.desktop\":[\"1707x1067\",\"9\",\"16\"],\"desktop:/org.telegram.desktop.desktop\":[\"1707x1067\",\"7\",\"0\"],\"desktop:/qq.desktop\":[\"1707x1067\",\"3\",\"0\"],\"desktop:/rstudio.desktop\":[\"1707x1067\",\"0\",\"7\"],\"desktop:/steam.desktop\":[\"1707x1067\",\"1\",\"16\"],\"desktop:/trash:⁄.desktop\":[\"1707x1067\",\"1\",\"0\"],\"desktop:/wechat.desktop\":[\"1707x1067\",\"4\",\"0\"],\"desktop:/wemeetapp.desktop\":[\"1707x1067\",\"9\",\"0\"],\"desktop:/wps-office-prometheus.desktop\":[\"1707x1067\",\"6\",\"0\"],\"desktop:/哔哩哔哩\":[\"1707x1067\",\"5\",\"0\"],\"desktop:/终末地\":[\"1707x1067\",\"2\",\"16\"],\"desktop:/鹰角启动器.desktop\":[\"1707x1067\",\"3\",\"16\"]}",
+                    "iconSize": "2",
+                    "lastResolution": "1707x1067",
+                    "positions": "{\"1707x1067\":[\"10\",\"17\",\"desktop:/android-studio-canary.desktop\",\"0\",\"9\",\"desktop:/wechat.desktop\",\"4\",\"0\",\"desktop:/wps-office-prometheus.desktop\",\"6\",\"0\",\"desktop:/trash:⁄.desktop\",\"1\",\"0\",\"desktop:/Marvel Spider-Man\",\"4\",\"16\",\"desktop:/鹰角启动器.desktop\",\"3\",\"16\",\"desktop:/哔哩哔哩\",\"5\",\"0\",\"desktop:/rstudio.desktop\",\"0\",\"7\",\"desktop:/steam.desktop\",\"1\",\"16\",\"desktop:/qq.desktop\",\"3\",\"0\",\"desktop:/computer.desktop\",\"0\",\"0\",\"desktop:/open-orpheus.desktop\",\"9\",\"16\",\"desktop:/code.desktop\",\"0\",\"8\",\"desktop:/HMCL.desktop\",\"0\",\"16\",\"desktop:/wemeetapp.desktop\",\"9\",\"0\",\"desktop:/终末地\",\"2\",\"16\",\"desktop:/org.telegram.desktop.desktop\",\"7\",\"0\",\"desktop:/feishu.desktop\",\"8\",\"0\",\"desktop:/firefox.desktop\",\"2\",\"0\"],\"1920x1080\":[\"10\",\"20\",\"desktop:/qq.desktop\",\"3\",\"0\",\"desktop:/wechat.desktop\",\"4\",\"0\",\"desktop:/org.telegram.desktop.desktop\",\"8\",\"0\",\"desktop:/Marvels Spider-Man Remastered.desktop\",\"4\",\"16\",\"desktop:/HMCL.desktop\",\"0\",\"16\",\"desktop:/wemeetapp.desktop\",\"0\",\"1\",\"desktop:/trash:⁄.desktop\",\"1\",\"0\",\"desktop:/哔哩哔哩\",\"5\",\"0\",\"desktop:/feishu.desktop\",\"7\",\"0\",\"desktop:/鹰角启动器.desktop\",\"3\",\"16\",\"desktop:/code.desktop\",\"9\",\"0\",\"desktop:/computer.desktop\",\"0\",\"0\",\"desktop:/steam.desktop\",\"1\",\"16\",\"desktop:/终末地\",\"2\",\"16\",\"desktop:/wps-office-prometheus.desktop\",\"6\",\"0\",\"desktop:/firefox.desktop\",\"2\",\"0\"]}",
+                    "sortMode": "-1"
+                },
+                "/Wallpaper/com.github.catsout.wallpaperEngineKde/General": {
+                    "PauseMode": "4",
+                    "SteamLibraryPath": "file:///media/skyshadowhero/D/SteamLibrary/steamapps/common/wallpaper_engine/assets"
+                },
+                "/Wallpaper/org.kde.slideshow/General": {
+                    "Image": "file:///media/skyshadowhero/E/Workplace/Minecraft/screenshots/2026-03-13_19.09.29.png",
+                    "SlideInterval": "600",
+                    "SlidePaths": "/media/skyshadowhero/E/Workplace/Minecraft/screenshots/"
+                }
+            },
+            "wallpaperPlugin": "org.kde.slideshow"
+        },
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "1",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "561",
+                            "popupWidth": "635"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "540",
+                            "DialogWidth": "720"
+                        },
+                        "/General": {
+                            "favoritesPortedToKAstats": "true",
+                            "forceDarkMode": "false",
+                            "highlightNewlyInstalledApps": "false",
+                            "icon": "deepin-launcher",
+                            "showActionButtonCaptions": "false",
+                            "systemFavorites": "suspend\\,hibernate\\,reboot\\,shutdown"
+                        },
+                        "/Shortcuts": {
+                            "global": "Alt+F1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/General": {
+                            "iconSpacing": "0",
+                            "launchers": "applications:kitty.desktop,applications:firefox.desktop"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.marginsseparator"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "247",
+                            "popupWidth": "304"
+                        },
+                        "/Appearance": {
+                            "autoFontAndSize": "false",
+                            "fontFamily": "MiSans VF",
+                            "fontSize": "8",
+                            "fontStyleName": "Regular",
+                            "fontWeight": "400"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "433",
+                            "popupWidth": "432"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.showdesktop"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 2.5555555555555554,
+            "hiding": "normal",
+            "location": "bottom",
+            "maximumLength": 94.83333333333333,
+            "minimumLength": 94.83333333333333,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
